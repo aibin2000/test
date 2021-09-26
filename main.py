46bcc507bc2baef2dd2be559cf3ef1db18afcc5a
peterai <aibin2000@hotmail.com>
2021-09-27 00:34:29 +1000

    test 2

---
+++ main.py
@@ -0,0 +1,9 @@
+# This is a test to run core python 3 code
+x1 = "she " # global variable
+print(f"I have a secret to say {x1 * 3}is a bitch!")
+
+print("simple text")
+print("another statement!")
+
+print("demo 1")
+print("another test")
